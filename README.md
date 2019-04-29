c783f0e51b5fe342d93f450c8b429d3dd070ff83
FlaxBear <ht14a032@oecu.jp>
2019-04-29 11:15:13 +0900

    Update microbit program and README

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
-Name
+micro:Wether
 ====
-
-Overview
+https://user-images.githubusercontent.com/22972451/56873778-d80aa080-6a6f-11e9-9f66-a9c27f06cd47.png
 
 ## Description
 
--- micro_bit/microbit.js
+++ micro_bit/microbit.js
@@ -1,1 +1,12 @@
-bluetooth.startLEDService();
+bluetooth.onBluetoothDisconnected(function () {
+    basic.showIcon(IconNames.Sad)
+    basic.pause(500)
+    basic.clearScreen()
+})
+bluetooth.onBluetoothConnected(function () {
+    basic.showIcon(IconNames.Happy)
+    basic.pause(500)
+    basic.clearScreen()
+})
+bluetooth.startLEDService()
+bluetooth.startButtonService()
